2b925fdf3d9fbedf7c7358c5703316311d7e150f
Lee kiseop <dlrltjq1004@gmail.com>
2020-08-18 15:40:39 +0900

    Added login credentials

--- server.js
+++ server.js
@@ -21,34 +21,6 @@ db.select('*')
 const app = express();
 app.use(bodyParser.json());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      password: 'cookies',
-      email: 'john@gmail.com',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      password: 'bananas',
-      email: 'sally@gmail.com',
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-  login: [
-    {
-      id: '987',
-      has: '',
-      email: 'john@gmail.com',
-    },
-  ],
-};
-
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -57,49 +29,59 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-  bcrypt.compare(
-    'apples',
-    '$2a$10$8n7CXjuPkdeDqhmEtp48OO1abpvPzAY0Fim8m3k6H170WBjC7Uw0i',
-    function (err, res) {
-      // res == true
-      console.log('first guess', res);
-    }
-  );
-  bcrypt.compare(
-    'veggies',
-    '$2a$10$cJf6kiMPFOxtw8MUvT/hEuxOwJsAy3gTFWPcF0qnryryGlta1f9oq',
-    function (err, res) {
-      // res = false
-      console.log('second guess', res);
-    }
-  );
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      console.log(isValid);
+      if (isValid) {
+        return db
+          .select('*')
+          .from('login')
+          .where('email', '=', req.body.email)
+          .then((user) => {
+            console.log(user);
+            res.json(user[0]);
+          })
+          .catch((err) => {
+            res.status(400).json('unable to get user');
+          });
+      } else {
+        res.status(400).json('wron credentials');
+      }
+    })
+    .catch((err) => {
+      res.status(400).json('wron credentials');
+    });
 });
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  bcrypt.hash(password, null, null, function (err, hash) {
-    // Store hash in your password DB.
-    console.log(hash);
-  });
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json('unable to register'));
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.lollback);
+  }).catch((err) => res.status(400).json('unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
