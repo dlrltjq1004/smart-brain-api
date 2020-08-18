aa808951d60e45c63c7daf8187890487b21a0dc0
Lee kiseop <dlrltjq1004@gmail.com>
2020-08-18 14:13:40 +0900

    database setting and user register update

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'test',
+    database: 'smart_brain',
+  },
+});
+
+db.select('*')
+  .from('users')
+  .then((data) => {
+    console.log(data);
+  });
 
 const app = express();
 app.use(bodyParser.json());
@@ -74,29 +91,34 @@ app.post('/register', (req, res) => {
     // Store hash in your password DB.
     console.log(hash);
   });
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.json(database.users[database.users.length - 1]);
+  db('users')
+    .returning('*')
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date(),
+    })
+    .then((user) => {
+      res.json(user[0]);
+    })
+    .catch((err) => res.status(400).json('unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(400).json('not found');
-  }
+  db.select('*')
+    .from('users')
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('Not found');
+      }
+    })
+    .catch((err) => {
+      res.status(400).json('error getting user');
+    });
 });
 
 app.put('/image', (req, res) => {
